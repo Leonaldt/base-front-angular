2145e5d841a915fe1a9cf4442488128f690ab59d
Leônidas Lima <leonidas.lima@itbam.org.br>
2021-04-15 22:34:20 -0400

    fix: removed unnecessary files

--- src/app/external/repository/user/user-repository.ts
+++
@@ -1,7 +0,0 @@
-
-import { Observable } from 'rxjs';
-import { UserEntity } from './user-entity';
-
-export abstract class UserRepository {
-    abstract register(UserData: UserEntity): Observable<UserEntity>
-}
